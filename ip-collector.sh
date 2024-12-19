568c78ccc43a5c2fe17d1716e07b6fe68cf38970
bruch-alex <alex.bruch@tutanota.com>
2024-12-19 16:30:57 +0100

    read .log file generated by 3x-ui instead of syslogs

--- ip-collector.sh
+++ ip-collector.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 NETWORKS_FILE=networks.txt
+CONTAINER="3x-ui"
+LOGS_FILE_PATH="access.log"
 
 # Array to hold the IP groups by network
 declare -A networks
 declare last_network
+
+# Check if .log file exists
+if ! docker exec $CONTAINER test -e $LOGS_FILE_PATH; then
+    echo ".log file not found"
+    echo "U need to enable logs in 3x-ui webpanel"
+    exit
+fi
 
 if [ -e "$NETWORKS_FILE" ]; then
     echo "Reading existing ip's from $NETWORKS_FILE"
@@ -16,23 +25,15 @@ if [ -e "$NETWORKS_FILE" ]; then
 fi
 
 # Grep and process IPs
-echo "Reading logs..."
-
-mapfile -t ips < <(grep xray /var/log/syslog | cut -d ' ' -f 9 | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u)
-#mapfile -t ips < <(docker exec -it 3x-ui grep -Eo 'from ([0-9]{1,3}\.){3}[0-9]{1,3}' access.log | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u)
+echo "Reading 3x-ui logs..."
+mapfile -t ips < <(docker exec -it 3x-ui grep 'email:' $LOGS_FILE_PATH | grep -Eo 'from ([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u)
 
 # Initialize counter for new IP's
 new_ip_counter=0;
 skipped_ip_counter=0;
 echo "Working with IP's..."
 for ip in "${ips[@]}"; do
-    
-    if [ "$(geoiplookup "$ip" | cut -d ' ' -f 4)" != "RU," ]; then
-        #echo "skipping ip: $ip"
-        ((skipped_ip_counter++)) 
-        continue
-    fi
-    
+        
     # Convert ip from 192.168.1.1 to 192.168.0.0/16
     current_network=$(echo "$ip" | sed 's/\([0-9]*\.[0-9]*\)\.[0-9]*\.[0-9]*/\1.0.0\/16/' | sed 's/ //g')
 
