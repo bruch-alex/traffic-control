83b887bb5403359a0cacbfb912f55e698bdce7ec
bruch-alex <alex.bruch@tutanota.com>
2024-12-21 02:15:16 +0100

    refactor ip collector and add .gitignore file

--- ip-collector.sh
+++ ip-collector.sh
@@ -14,47 +14,45 @@ if ! docker exec $CONTAINER test -e $LOGS_FILE_PATH; then
     exit
 fi
 
+# Check and read existing networks file to an array
 if [ -e "$NETWORKS_FILE" ]; then
-    echo "Reading existing ip's from $NETWORKS_FILE"
-    ip_from_file_counter=0
-    while IFS= read -r ip; do
+    echo "Reading $NETWORKS_FILE"
+    mapfile -t temp < "$NETWORKS_FILE"
+    for ip in "${temp[@]}"; do
         networks["$ip"]=0
-        ((ip_from_file_counter++))
-    done < $NETWORKS_FILE
-    echo "Found $ip_from_file_counter ip's in $NETWORKS_FILE"
+    done
+    echo "Found ${#networks[@]} ip in $NETWORKS_FILE"
 fi
 
-# Grep and process IPs
+# Grep ip from logs
 echo "Reading 3x-ui logs..."
 mapfile -t ips < <(docker exec -it 3x-ui grep 'email:' $LOGS_FILE_PATH | grep -Eo 'from ([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u)
+echo "Found ${#ips[@]} ip in 3x-ui logs"
 
-# Initialize counter for new IP's
+# Initialize counter for new and skipped ip and iterate through logs
 new_ip_counter=0;
 skipped_ip_counter=0;
-echo "Working with IP's..."
+echo "Converting ip to network"
 for ip in "${ips[@]}"; do
         
     # Convert ip from 192.168.1.1 to 192.168.0.0/16
-    current_network=$(echo "$ip" | sed 's/\([0-9]*\.[0-9]*\)\.[0-9]*\.[0-9]*/\1.0.0\/16/' | sed 's/ //g')
+    current_network=$(echo "$ip" | sed 's/\([0-9]*\.[0-9]*\)\.[0-9]*\.[0-9]*/\1.0.0\/16/')
 
-    # Check if it's null (or unset)
+    # Check if ip exists and add to array
     if [[ -v networks["$current_network"] ]]; then
-        #echo "$current_network exists, skipping..."
+        ((skipped_ip_counter++))
         continue
     else
-        #echo "$current_network does not exist, adding ip."
         networks["$current_network"]=0
-        echo "$current_network" >> $NETWORKS_FILE
         ((new_ip_counter++))
     fi
 done
 
-echo "$new_ip_counter new ip's have been added to the $NETWORKS_FILE"
-echo "$skipped_ip_counter ip's were skipped"
+# Overwriting networks file
+> $NETWORKS_FILE
+for ip in "${!networks[@]}"; do
+    echo "$ip" >> $NETWORKS_FILE 
+done
 
-#Delete duplicates
-echo "Elemets before cleanup: $(wc -l $NETWORKS_FILE)"
-echo "Starting cleanup"
-sudo grep -Eo '([0-9]{1,3}\.){2}0\.0' $NETWORKS_FILE | sort -u > $NETWORKS_FILE.tmp && sudo mv $NETWORKS_FILE.tmp $NETWORKS_FILE
-echo "Cleanup finished"
-echo "Elemets after cleanup: $(wc -l $NETWORKS_FILE)"
+echo "$new_ip_counter new networks have been added to the $NETWORKS_FILE"
+echo "$skipped_ip_counter networks were skipped"
