481e4caf46655b8aa8169893178ceec77ac1331f
bruch-alex <alex.bruch@tutanota.com>
2025-06-12 17:53:20 +0200

    update script

--- ip-collector.sh
+++ ip-collector.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 NETWORKS_FILE=networks.txt
-CONTAINER="3x-ui"
-LOGS_FILE_PATH="access.log"
+LOGS_FILE_PATH="/var/log/xray-connections.log"
 
 # Array to hold the IP groups by network
 declare -A networks
 declare last_network
 
 # Check if .log file exists
-if ! docker exec $CONTAINER test -e $LOGS_FILE_PATH; then
+if ! test -e $LOGS_FILE_PATH; then
     echo ".log file not found"
-    echo "U need to enable logs in 3x-ui webpanel"
     exit
 fi
 
@@ -18,16 +16,16 @@ fi
 if [ -e "$NETWORKS_FILE" ]; then
     echo "Reading $NETWORKS_FILE"
     mapfile -t temp < "$NETWORKS_FILE"
-    for ip in "${temp[@]}"; do
-        networks["$ip"]=0
+    for network in "${temp[@]}"; do
+        networks["$network"]=0
     done
     echo "Found ${#networks[@]} ip in $NETWORKS_FILE"
 fi
 
-# Grep ip from logs
-echo "Reading 3x-ui logs..."
-mapfile -t ips < <(docker exec -it 3x-ui grep 'email:' $LOGS_FILE_PATH | grep -Eo 'from ([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u)
-echo "Found ${#ips[@]} ip in 3x-ui logs"
+# Grep ips from logs
+echo "Reading xray logs..."
+mapfile -t ips < <(awk '{print $5}' "$LOGFILE" | grep -oP '(?<=::ffff:)[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(?=\])' | sort -u)
+echo "Found ${#ips[@]} ip in logs"
 
 # Initialize counter for new and skipped ip and iterate through logs
 new_ip_counter=0;
