1a247a3611ad172e089965d77bb63347ef331bb5
bruch-alex <alex.bruch@tutanota.com>
2025-06-12 17:34:19 +0200

    update logger

--- xray_logger.sh
+++ xray_logger.sh
@@ -2,9 +2,6 @@
 
 LOGFILE="/var/log/xray-connections.log"
 
-while true; do
   echo "==== $(date) ====" >> "$LOGFILE"
   ss -ntp | grep 'xray' >> "$LOGFILE"
   echo "" >> "$LOGFILE"
-  sleep 5
-done
